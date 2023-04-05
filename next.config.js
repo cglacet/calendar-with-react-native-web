3c37140368aab9f1c13eb3af254e4cb750619328
Christian Glacet <christian.glacet@gmail.com>
2023-04-05 18:20:42 +0200

    Add react-native-calendars

--- next.config.js
+++ next.config.js
@@ -1,18 +1,30 @@
+const withTM = require("next-transpile-modules"); // pass the modules you would like to see transpiled
+
+const modulesToTranspile = [
+    "react-native-calendars",
+    "react-native-swipe-gestures",
+    "recyclerlistview",
+];
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
-      'react-native$': 'react-native-web',
-    }
-    config.resolve.extensions = [
-      '.web.js',
-      '.web.jsx',
-      '.web.ts',
-      '.web.tsx',
-      ...config.resolve.extensions,
-    ]
-    return config
-  },
-}
+const configuration = {
+    transpilePackages: modulesToTranspile,
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...(config.resolve.alias || {}),
+            // Transform all direct `react-native` imports to `react-native-web`
+            "react-native$": "react-native-web",
+        };
+        config.resolve.extensions = [
+            ".web.js",
+            ".web.jsx",
+            ".web.ts",
+            ".web.tsx",
+            ...config.resolve.extensions,
+            "...",
+        ];
+        return config;
+    },
+};
+
+module.exports = withTM(modulesToTranspile)(configuration);
